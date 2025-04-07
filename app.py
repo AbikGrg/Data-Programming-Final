05b269c28eb41c91396412fc0b063b6e3c705f34
soedez <soedezh@gmail.com>
2025-04-07 19:50:58 -0400

    Minor changes

--- app.py
+++ app.py
@@ -23,5 +23,5 @@ with app.app_context():
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()  # Create tables if they don't exist
-    app.run(debug=True)
+        db.create_all()
+    app.run(debug=True, host='127.0.0.1', port=5000)
